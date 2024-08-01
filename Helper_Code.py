0ab5292e5adcb4b3a2bbcbe45004315c1d79910f
sanskar-agrawal2018 <sanskaragrawal1903@gmail.com>
2024-08-01 11:51:40 +0530

    Mail

--- Helper_Code.py
+++ Helper_Code.py
@@ -1,1 +1,6 @@
-print('sanskar')
+Entity Split for HKG:
+Implement a 60/40 split for the honking entity in the new entity HKG.
+Default Values for E2k Accounts and Portfolios:
+Haun provides default values for E2k accounts and portfolios. Use the E2k account for GMISt4F1, and handle cases where a new account is not present in the mapping table.
+BDR_Product_Code Population:
+Vijetha will explain how and where to populate the BDR_Product_Code.
