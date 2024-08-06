2554e1d8426d1eb19b3d9abb7e6f31fb64a0f3d6
sanskar-agrawal2018 <sanskaragrawal1903@gmail.com>
2024-08-06 18:13:35 +0530

    Linux curl

--- Helper_Code.py
+++ Helper_Code.py
@@ -1,6 +1,1 @@
-E2k Mapping Table Update:
-The DFIN team will provide the FINGPA team with an updated E2k mapping table.
-The provided table will contain only two columns.
-We need clarity on the preferred approach for updating the existing data:
-Truncate and Load: Should we truncate the existing table and load the new data entirely?
-Row-Level Update: Alternatively, should we only add or update specific rows based on the new data?
+curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
