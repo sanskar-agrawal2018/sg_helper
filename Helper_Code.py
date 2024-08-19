5e9e2bfc38b6b4b40112aa9f1ca3aa7763b28cbd
sanskar-agrawal2018 <59973565+sanskar-agrawal2018@users.noreply.github.com>
2024-08-19 12:52:37 +0530

    Update Helper_Code.py

--- Helper_Code.py
+++ Helper_Code.py
@@ -1,2 +1,28 @@
-FIS Decom 
-Worked on decommission the FIS vendor application, which required expensive maintenance and enhancements from an external vendor, and to streamline operations by integrating its functionalities into existing systems.
+<UC_OBJECT OBJECT_TYPE="JOBS">
+    <JOB TITLE="SSH to Linux Server" RUN_MASK="0000000000000000" OH_SubType="UNIX">
+        <JBAgentName>YOUR_AGENT_NAME</JBAgentName>
+        <JBAgentType>UNIX</JBAgentType>
+        <JBAgentOsType>Unix</JBAgentOsType>
+        <JBMode>PROCESS</JBMode>
+        <JBProcess>
+            <![CDATA[
+#!/bin/bash
+
+# Variables
+REMOTE_USER="username"
+REMOTE_HOST="your.linux.server"
+COMMAND="ls -l /path/to/directory"
+
+# SSH Command Execution
+ssh -o StrictHostKeyChecking=no $REMOTE_USER@$REMOTE_HOST "$COMMAND"
+            ]]>
+        </JBProcess>
+    </JOB>
+    <ATTRIBUTES>
+        <Queue>CLIENT_QUEUE</Queue>
+        <Priority>000</Priority>
+        <MaxExecutionTime>00:30</MaxExecutionTime>
+        <TimeZone>UTC</TimeZone>
+        <Notification></Notification>
+    </ATTRIBUTES>
+</UC_OBJECT>
